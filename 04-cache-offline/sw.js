ed8cf63cbbfb89b20db7807c57e79943495f2ca4
Leonardo Medina <leonardo.medina@bayteq.com>
2019-10-13 04:24:53 -0500

    60 estrategia: cachhe with network fallback

--- 04-cache-offline/sw.js
+++ 04-cache-offline/sw.js
@@ -1,9 +1,9 @@
 
 // la URL a donde estoy accediendo
 // http://localhost/PWA/04-cache-offline/index.html
-
+const CACHE_NAME = 'cache-1'
 self.addEventListener('install', e=>{
-    const cacheProm = caches.open( 'cache-1' )
+    const cacheProm = caches.open( CACHE_NAME)
         .then(cache=>{
                 return cache.addAll([
                     '/PWA/04-cache-offline/',
@@ -30,12 +30,34 @@ cache only: una vez que se realiza la instalacion de la paguiana web, jamas regr
 
 self.addEventListener('fetch', e=>{
 
-    //catch only: esta es usada cuando queremos que toda la app es servida desde la cache
+    //1. catch only: esta es usada cuando queremos que toda la app es servida desde la cache
     //            es decir no haber peticion que acceda a la web
 
 
     //se va a todos los caches que esten en el dominio que coincida, ej si el servidor esta el index, busca en la cache el index
     // esta estrategia tiene ciertos problemas
     // si jamas actualizamos el sw, jamas se actualiza a la web
-    e.respondWith( caches.match( e.request ));
+    // e.respondWith( caches.match( e.request ));
+
+    //2. cache fallback then cache: intenta primero el cache y despues ve a la internet
+
+    const respuesta = caches.match( e.request )
+        .then( res=>{
+
+            if( res ) return res;
+
+            // NO existe el archivo
+            // tengo que ir a la web
+            console.log('no existe', e.request.url );
+
+            return fetch( e.request ).then( newRes =>{
+                    // vuelve a cargar un archivo al cache y lo actualiza (put)
+                    caches.open(CACHE_NAME)
+                        .then( cache=>{
+                            cache.put( e.request, newRes );
+                        });
+                    return newRes.clone();
+                });
+        });
+    e.respondWith( respuesta );
 })
