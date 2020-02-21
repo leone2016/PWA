93ebc960c5edca3e2a59811ecdaaa23ee18b87b3
Leonardo Medina <leoz.31@hotmail.com>
2020-02-21 04:21:39 -0500

    pasive course

--- 05-navegacion-offline/sw.js
+++ 05-navegacion-offline/sw.js
@@ -1,5 +1,5 @@
 // const CACHE_NAME = 'cache-1';
-const CACHE_STATIC_NAME  = 'static-v2';
+const CACHE_STATIC_NAME  = 'static-v1';
 const CACHE_DYNAMIC_NAME = 'dynamic-v1';
 const CACHE_INMUTABLE_NAME = 'inmutable-v1';
 
@@ -35,12 +35,13 @@ self.addEventListener('install', e => {
         .then( cache => {
 
             return cache.addAll([
-                '/PWA/04-cache-offline/',
+                '/PWA/05-navegacion-offline/',
                 'index.html',
                 'css/style.css',
                 'img/main.jpg',
                 'js/app.js',
-                'img/no-img.jpg'
+                'img/no-img.jpg',
+                'pages/offline.html'
             ]);
 
 
@@ -54,4 +55,30 @@ self.addEventListener('install', e => {
 
 });
 
+// se aplica la segunda estrategia
 
+self.addEventListener('fetch', e=>{
+    //2. cache fallback then cache: intenta primero el cache y despues ve a la internet
+
+    const respuesta = caches.match( e.request )// busca lo que sea que se este solicitando en la cache Eg. Bootstrap, imagenes etc
+        .then( res=>{
+
+            if( res ) return res // si existe retorna y hasta ahi llega
+
+            // NO existe el archivo
+            // tengo que ir a la web
+            //console.log('no existe', e.request.url );
+
+            // en el caso de que no exista la busca en la web (FETCH)
+            return fetch( e.request ).then( newRes =>{ // si lo encuentra tendriamos una nueva respuesta
+                    // vuelve a cargar un archivo al cache y lo actualiza (put)
+                    caches.open(CACHE_DYNAMIC_NAME) // y lo guarda en el Dinamic caches
+                        .then( cache=>{
+                            cache.put( e.request, newRes );
+                            limpiarCache(CACHE_DYNAMIC_NAME, CACHE_DYNAMIC_LIMIT );
+                        });
+                    return newRes.clone();
+                });
+        });
+    e.respondWith( respuesta );
+})
